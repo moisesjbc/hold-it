f6863f811497877174dd5eb9edc2a2eb3f0aaaab
Moisés J <moisesjbc@gmail.com>
2022-04-03 19:16:22 +0100

    Add exit button

--- godot-project/gui/main_menu/main_menu.gd
+++ godot-project/gui/main_menu/main_menu.gd
@@ -3,6 +3,9 @@ extends Control
 func _ready():
 	randomize()
 	get_tree().paused = false
+	
+	if OS.get_name() == "HTML5":
+		$margin_container/vbox_container/buttons_container/exit_button.visible = false
 
 func _on_play_button_pressed():
 	get_tree().change_scene("res://gameplay/main/main.tscn")
@@ -14,3 +17,7 @@ func _on_credits_menu_button_pressed():
 
 func _on_how_to_play_button_pressed():
 	get_tree().change_scene("res://gui/how_to_play_menu/how_to_play_menu.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
